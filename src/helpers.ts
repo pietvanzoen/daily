7d45309d4ccadc4305a548997f41c2504f725b51
pietvanzoen <hi@piet.me>
2024-03-31 20:57:48 +0200

    Format the helpers.ts file

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,19 +1,21 @@
-import MarkdownIt from 'markdown-it';
-const md = new MarkdownIt();
+import MarkdownIt from 'markdown-it'
+const md = new MarkdownIt()
 
 // Remember the old renderer if overridden, or proxy to the default renderer.
-const  defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
-  return self.renderToken(tokens, idx, options);
-};
+const defaultRender =
+  md.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options)
+  }
 
 md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
   // Add a new `target` attribute, or replace the value of the existing one.
-  tokens[idx].attrSet('target', '_blank');
+  tokens[idx].attrSet('target', '_blank')
 
   // Pass the token to the default renderer.
-  return defaultRender(tokens, idx, options, env, self);
-};
+  return defaultRender(tokens, idx, options, env, self)
+}
 
 export function renderMarkdown(text: string) {
-  return md.renderInline(text);
+  return md.renderInline(text)
 }
